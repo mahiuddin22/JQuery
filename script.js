f410b74a76a1b2c0454aa8dd73c5983485701f60
mahiuddin22 <mahiuddin.noyon@gmail.com>
2023-01-11 01:48:38 +0600

    Jquery initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// $("h4, p").html("This is the"+" <b>first</b> "+"JQuery test");
+
+$("#p1").append(".... Set After the content withoit replacing using <b><i>append()</i></b> method");
+$("#p2").prepend("Set Before the content withoit replacing using <b><i>prepend()</i></b> method.... ");
+
+let para1 = $("<p></p>").text("This is the paragraph before content");
+let para2 = $("<p></p>").text("This is the paragraph after content");
+
+$("#beforeafter").before(para1);
+$("#beforeafter").after(para2);
+
+// let myAttribute = document.querySelector("a").getAttribute("href");
+// console.log(myAttribute)
+// $("a").attr("href");
+// $("a").removeAttr("href");
+
+// document.querySelector("a").setAttribute("href","https://www.mahiuddin.epizy.com");
+
+$("a").attr("href","https://www.mahiuddin.epizy.com");
+$("#styling").css({
+    "backgroundColor":"#e74c3c",
+    "color":"#fff",
+    "borderRadius":"5px",
+    "boxShadow":"5px 4px 5px #95a5a6",
+    "padding" : "4px 0 4px 10px"
+});
+//We can use any pre customised class using the method addClass("example_name");
+let styling1 = $("<h1></h1>").text("Added from JQuery page").css({
+    "backgroundColor":"#3498db",
+    "color":"#fff",
+    "borderRadius":"5px",
+    "boxShadow":"5px 4px 5px #95a5a6",
+    "padding" : "4px 0 4px 10px"
+});
+$("#styling").after(styling1);
